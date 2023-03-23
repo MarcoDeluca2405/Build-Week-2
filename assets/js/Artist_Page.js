bb332a6349cf302bc50d42d13a5cc24aea94cfe8
MarcoDeLuca2405 <Marco_De_luca2405@hotmail.com>
2023-03-23 14:37:28 +0100

    mod Marco

--- assets/js/Artist_Page.js
+++ assets/js/Artist_Page.js
@@ -1,29 +1,42 @@
 let urlid = new URLSearchParams(window.location.search).get("id")
 let urlprincipale = "https://striveschool-api.herokuapp.com/api/deezer/artist/";
 let refernzacarte = document.getElementById("card")
+
 const cercaALBUM =async ()=>{
     let responsive = await fetch(urlprincipale+urlid)
     let risposta = await responsive.json();
-    let conferma = await risposta;
-    console.log(conferma)
-    console.log(conferma.name)
-    conferma.forEach((el)=>{
-        scrivi(el)
-        
-    })
+  let conferma=await risposta;
+  
+    console.log(conferma.tracklist);
+    let getalbum=await conferma.tracklist;
+    album(getalbum);
+  
 }
+
+
+const album= async (getalbum) =>{
+  let responsive = await fetch(getalbum);
+  let risposta = await responsive.json();
+  let getData=await risposta.data;
+  console.log(getData);
+  getData.forEach(element => {
+    scrivi(element);
+  });
+}
+
+
 cercaALBUM()
 function scrivi (Scritta){
     refernzacarte.innerHTML += `   <div class="col me-5 mb-5 ">
    <div class="d-flex justify-content-between ">
      <div class="d-flex ms-4 align-items-top">
        <p>1</p>
-         <img class="ms-4 me-2"src="${Scritta.picture_small}" width="30px" height="30px"  alt="">
-   <p>ocean aventure</p>
+         <img class="ms-4 me-2"src="${Scritta.album.cover_small}" width="30px" height="30px"  alt="">
+   <p>${Scritta.title}</p>
      </div>
    
-   <p>290909</p>
-   <p>3:12</p>
+   <p>${Scritta.rank}</p>
+   <p>${Scritta.duration}s</p>
    </div>
    </div>` 
 }
