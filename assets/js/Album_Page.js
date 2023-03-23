6302a268c48661c1335c10d3ef0a99bdb3a35691
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-03-23 11:40:55 +0100

    mm

--- assets/js/Album_Page.js
+++ assets/js/Album_Page.js
@@ -1,9 +1,35 @@
 
+
+    getDominantImageColor = ()=>{
+        // get the image
+        let sourceImage = document.querySelector("immagineAlbum");
+        // get the background element
+        console.log(sourceImage)
+        let background = document.querySelector("#due");
+        // initialize colorThief
+        let colorThief = new ColorThief();
+        // get color palette
+        let color = colorThief.getColor(sourceImage);
+        // set the background color
+        background.getElementsByClassName.backgroundcolor = "rgb(" + color + ")";
+    }
+    
+    
+
+
+
+
 Lista = document.getElementById('lista');
 let ID = new URLSearchParams(window.location.search).get('id');
 console.log(ID)
 
 console.log(Lista);
+
+urlprova = "https://striveschool-api.herokuapp.com/api/deezer/artist/412"
+'https://striveschool-api.herokuapp.com/api/deezer/album/75621062'
+
+
+
 
 
 
@@ -11,25 +37,31 @@ console.log(Lista);
 
 
 async function fetchata(){
-    const response = await fetch('https://striveschool-api.herokuapp.com/api/deezer/album/75621062')
+    const response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${ID}`)
     const oggetti = await response.json();
     console.log(oggetti)
     console.log((oggetti.tracks).data)
     console.log(oggetti.title)
 
+    Controllo((oggetti.artist).name)
     fillpage((oggetti.tracks).data)
-    document.getElementById('immagineAlbum').innerHTML = `<img src="${oggetti.cover_big}" class="img-fluid my-3 mx-3 image_t" alt="..."></img>`
+    document.getElementById('immagineAlbum').innerHTML = `<img src="${oggetti.cover_big}" id=bigCover" class="img-fluid my-3 mx-3 image_t" alt="..."></img>`
     fillpage2((oggetti))
-    
-        
-    
+    getDominantImageColor();
 }
+
+console.log()
+  
+
+
 
 function fillpage2(params) {
     titolo = params.title;
     document.getElementById('titotloAlbum').innerHTML = titolo;
     document.getElementById('autoreAlbum').innerText = params.artist.name;
 }
+
+
 
 
 fetchata();
@@ -44,7 +76,7 @@ function fillpage(cirio){
         cico = eval([index+1])
 
         Lista.innerHTML += `<div class="d-flex justify-content-between rounded hoverAlbum" id="elementlist" onmouseover=hoverh(${[index]}) onmouseout=hoverh(${[index]})>
-        <div class="d-flex"><span class="numberAlbum pt-2" >${cico}</span> &nbsp; &nbsp;<span class="row"><b>${titolo}</b> <span><a href="">${cirio[index].artist.name}</a></span> </span> </div> 
+        <div class="d-flex"><span class="numberAlbum pt-2 my-3" >${cico}</span> &nbsp; &nbsp;<span class="row"><b>${titolo}</b> <span><a href="">${cirio[index].artist.name}</a></span> </span> </div> 
                 <div  class="d-flex"> <div id="cuoricino${index}" class="me-3 pt-2 d-none"> 
                     <i id="cuorepieno${index}" onclick=riempi(${index}) class="bi bi-heart"></i> 
                             
@@ -72,6 +104,37 @@ function riempi(par) {
 function hoverh(par) {
     let cuore = document.getElementById(`cuoricino${par}`);
        cuore.classList.toggle('d-none');
+}
+
+async function Controllo(params) {
+    const response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${params}`)
+    const oggetto = await response.json();
+    const oggettovero =   oggetto.data;
+     console.log(oggettovero)
+
+     function randomizer(){
+       numerorand = Math.floor(Math.random() * oggettovero.length)
+       console.log(numerorand)
+       return numerorand
+     }
+
+     for (let index = 0; index < 6; index++) {
+        
+        console.log(index)
+        document.getElementById('altriAlbum').innerHTML += 
+        `
+         
+        <a href="Album_Page.html?id=${(oggettovero[index].album).id}" class="card cartecanz hoveraggio col-2 my-2" style="width: 12rem;">
+               <img src="${(oggettovero[index].album).cover}" class="card-img-top mt-2 " alt="...">
+               <div class="card-body">
+                  <p class="card-text"><b class="d-block">${(oggettovero[index].title_short)}</b> <span>${(oggettovero[index].cover)}</span></p>
+               </div>
+            </a> 
+     `;
+        
+     }
+    
+   
 }
 
 
